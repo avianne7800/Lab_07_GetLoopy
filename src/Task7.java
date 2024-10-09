bbe088ff1a0fdaecf9f487a455bd31ebe1435f91
avianne7800 <avianne7800@gmail.com>
2024-10-09 15:23:30 -0400

    Task 7 finished

---
+++ src/Task7.java
@@ -0,0 +1,12 @@
+public class Task7 {
+    public static void main(String[] args){
+        for (int num = 0; num < 5; num++)
+        {
+            for(int num2 = 0; num2 < 6; num2++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
