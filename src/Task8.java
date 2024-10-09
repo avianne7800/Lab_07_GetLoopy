9aad486def3f22fd541ae2fd609853ba64268eb0
avianne7800 <avianne7800@gmail.com>
2024-10-09 15:51:02 -0400

    start of Task 8

---
+++ src/Task8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Task8 {
+    public static void main(String[] args){
+        //initializing variables
+        Scanner in = new Scanner(System.in);
+        boolean done = false;
+
+        //start of the loop
+        System.out.println("Roll the dice?");
+            do{
+                //input for YES or NO
+                if(in.hasNext()) {
+
+                }
+            }while(!done);
+        }
+    }
+}
