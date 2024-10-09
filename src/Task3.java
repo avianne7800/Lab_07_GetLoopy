873966a45f108c9be5d5c754682f92ef9d37b4f7
avianne7800 <avianne7800@gmail.com>
2024-10-09 14:55:05 -0400

    Task 3 finished

---
+++ src/Task3.java
@@ -0,0 +1,10 @@
+public class Task3 {
+    public static void main(String[] args){
+        for (int count = 0; count < 18;)
+        {
+            count = count + 3;
+            System.out.print(count + ", ");
+
+        }
+    }
+}
