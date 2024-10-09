b6644e09f3c1a31abe8e47f041ca3916f61657c1
avianne7800 <avianne7800@gmail.com>
2024-10-09 14:54:20 -0400

    Task 2 finished

---
+++ src/Task2.java
@@ -0,0 +1,10 @@
+public class Task2 {
+    public static void main(String[] args){
+        for (int count = 31; count > 0;)
+        {
+            count = count - 1;
+            System.out.print(count + ", ");
+
+        }
+    }
+}
